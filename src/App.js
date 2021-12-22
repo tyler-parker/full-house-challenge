fe5a5cabc246960cfb099d772aacf93d286cc065
Tyler Parker <tparker42d2@gmail.com>
2021-12-21 18:53:04 -0700

    getting successful response from deck api, moving to CardDeck component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios'
 import {
   ChakraProvider,
   Box,
@@ -8,30 +9,39 @@ import {
   Code,
   Grid,
   theme,
+  Button
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 
 function App() {
+  const [ deck, setDeck ] = useState()
+  const [ gameInit, setGameInit ] = useState(false)
+
+  const getDeck = async () => {
+    await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+      .then(res => setDeck(res.data))
+      .catch(err => console.error(err))
+    setGameInit(prevState => !prevState)
+    console.log(deck);
+    console.log(deck.deck_id);
+    console.log(gameInit);
+  }
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+            {/* Deck goes here */}
+            <Grid templateColumns='repeat(1fr, 5)'>
+              {/* dealt cards go here */}
+            </Grid>
+            {
+              // !gameInit ?
+                <Button onClick={getDeck}>Shuffle Deck</Button>
+              // : <></>
+            }
           </VStack>
         </Grid>
       </Box>
---
+++ src/components/CardDeck.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import axios from 'axios'
+import {
+    Box
+} from '@chakra-ui/react'
+
+export default function CardDeck() {
+    return (
+        <Box>
+            
+        </Box>
+    )
+}
